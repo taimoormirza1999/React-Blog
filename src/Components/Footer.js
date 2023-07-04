8d77ef8fa55b79a2212294f164aa84dae9426cb7
taimoorhamza1999 <74351945+taimoormirza1999@users.noreply.github.com>
2023-07-04 17:12:18 +0400

    text change in footer

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -28,7 +28,7 @@ export default function Footer() {
         </div>
   
         <div className="col-6 col-md-2 mb-3">
-          <h5>Section</h5>
+          <h5>Sections</h5>
           <ul className="nav flex-column">
             <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-body-secondary">Home</a></li>
             <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-body-secondary">Features</a></li>
